fe04b40069d4cac1fea7d5f0532e3c616968d6fc
ritik <ritikraw@gmail.com>
2024-02-15 18:34:08 +0530

    close menu on document click

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import styles from "./navbar.module.scss";
 
@@ -8,23 +8,53 @@ const Navbar = () => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleItemClick = (index: number) => {
+  const handleItemClick = (index: number, value: any) => {
+    console.log(value);
     setActiveIndex(index);
   };
 
-  const navItems = ["HOME", "DESTINATION", "CREW", "TECHNOLOGY"];
+  const navItems = [
+    { label: "HOME", value: "home" },
+    { label: "DESTINATION", value: "destination" },
+    { label: "CREW", value: "crew" },
+    { label: "TECHNOLOGY", value: "technology" },
+  ];
 
-  const navItemsBtn = navItems.map((label: string, index: number) => {
-    return (
-      <NavItem
-        key={label}
-        count={index}
-        activeIndex={activeIndex}
-        onClick={() => handleItemClick(index)}
-        label={label}
-      />
-    );
-  });
+  const navItemsBtn = navItems.map(
+    ({ label, value }: { label: string; value: any }, index: number) => {
+      return (
+        <NavItem
+          key={label}
+          count={index}
+          activeIndex={activeIndex}
+          onClick={() => handleItemClick(index, value)}
+          label={label}
+          value={value}
+        />
+      );
+    }
+  );
+
+  const menuBtnRef = useRef<HTMLImageElement>(null);
+  const menuRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuBtnRef.current &&
+        !menuBtnRef.current.contains(event.target as Node) &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node)
+      ) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <header className={styles.header}>
@@ -36,19 +66,23 @@ const Navbar = () => {
       </div>
       <img
         className={styles.menu_btn}
+        ref={menuBtnRef}
         src={
           isMenuOpen
             ? "/assets/shared/icon-close.svg"
             : "/assets/shared/icon-hamburger.svg"
         }
         alt="menu-button"
-        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        onClick={() => {
+          setIsMenuOpen(!isMenuOpen);
+          console.log("click");
+        }}
       />
       <nav className={styles.menu}>
         <ul>{navItemsBtn}</ul>
       </nav>
       {isMenuOpen && (
-        <nav className={styles.mobile_menu}>
+        <nav className={styles.mobile_menu} ref={menuRef}>
           <ul>{navItemsBtn}</ul>
         </nav>
       )}
