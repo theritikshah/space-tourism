70adfaddae9aa0953c9c444060ee1f317d53eb89
ritik <ritikraw@gmail.com>
2024-02-16 16:35:00 +0530

    load data

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+async function getData() {
+  try {
+    const response = await fetch(`${process.env.HOST_ADDRESS}/data.json`);
+    const data = await response.json();
+    return { props: { data, name: "success" } };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return { props: { data: null } }; // or handle error accordingly
+  }
+}
 
-const page = () => {
-  return <div>page</div>;
-};
+export default async function Planets() {
+  const data = await getData();
 
-export default page;
+  console.log(data);
+  return (
+    <div>
+      <h1>Data</h1>
+      {data && <p>{JSON.stringify(data)}</p>}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`./data.json`);
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
