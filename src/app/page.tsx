8d3b744137eb58aec849b4a44e48b6b1ff0d6bde
Ritik <ritikraw@gmail.com>
2024-05-30 16:08:10 +0530

    Add global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className={styles.content}>
         <h5 className="subheading-1">SO, YOU WANT TO TRAVEL TO</h5>
         <h1>SPACE</h1>
-        <p style={{ color: "#D0D6F9", lineHeight: "3.2rem", margin: 0 }}>
+        <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
